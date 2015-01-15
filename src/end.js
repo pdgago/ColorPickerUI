9269edf07ef338baff57e77e105fa433eb5e0062
Pedro <pdgago@gmail.com>
2015-01-15 13:37:03 +0100

    config

---
+++ src/end.js
@@ -0,0 +1,9 @@
+/* jshint ignore:start */
+  if (typeof define === 'function' && define.amd) {
+    define(ColorPickerUi);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = ColorPickerUi;
+  }
+  this.ColorPickerUi = ColorPickerUi;
+}.call(window);
+/* jshint ignore:end */
---
+++ src/start.js
@@ -0,0 +1,5 @@
+/* jshint ignore:start */
+!function(context) {
+  'use strict';
+  var ColorPickerUi = {version: "0.0.0"};
+/* jshint ignore:end */
